4c677d4d209e47b0b8aa9e0a22334207363db4f4
mana.sasaki <xm80cv@insim.biz>
2022-12-21 14:57:28 +0900

    addTable

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,45 @@ const App = () => {
   return (
     <React.Fragment>
       <div className="app">
+        <div className="menu">
+          <Menu
+            name={items[0].name}
+            value={items[0].value}
+            setTotal={setTotal}
+          />
+          <Menu
+            name={items[1].name}
+            value={items[1].value}
+            setTotal={setTotal}
+          />
+          <Menu
+            name={items[2].name}
+            value={items[2].value}
+            setTotal={setTotal}
+          />
+          <Menu
+            name={items[3].name}
+            value={items[3].value}
+            setTotal={setTotal}
+          />
+          <Menu
+            name={items[4].name}
+            value={items[4].value}
+            setTotal={setTotal}
+          />
+        </div>
+
+        <div className="details">
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 450 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>商品</TableCell>
+                  <TableCell>値段</TableCell>
+                  <TableCell>数</TableCell>
+                  <TableCell align="right">金額</TableCell>
+                </TableRow>
+              </TableHead>
 
         <div className="menu">
            <Menu name = "Coffee" value= {480} setTotal={setTotal}/>
